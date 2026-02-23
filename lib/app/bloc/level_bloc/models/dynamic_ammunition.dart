f77284e8d03ecfb3fb282a92dcae55d389b0e7e3
Anton Zyryanov <antonzyryanov99@gmail.com>
2026-02-24 01:26:00 +0300

    fix: rare not destroyable enemy bug fix

--- lib/app/bloc/level_bloc/models/dynamic_ammunition.dart
+++ lib/app/bloc/level_bloc/models/dynamic_ammunition.dart
@@ -2,18 +2,28 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 
 class DynamicAmmunition extends Equatable {
-  const DynamicAmmunition({required this.center, required this.speed});
+  const DynamicAmmunition({
+    required this.center,
+    required this.speed,
+    this.previousCenter,
+  });
 
   final Offset center;
   final double speed;
+  final Offset? previousCenter;
 
-  DynamicAmmunition copyWith({Offset? center, double? speed}) {
+  DynamicAmmunition copyWith({
+    Offset? center,
+    double? speed,
+    Offset? previousCenter,
+  }) {
     return DynamicAmmunition(
       center: center ?? this.center,
       speed: speed ?? this.speed,
+      previousCenter: previousCenter ?? this.previousCenter ?? this.center,
     );
   }
 
   @override
-  List<Object?> get props => [center, speed];
+  List<Object?> get props => [center, speed, previousCenter];
 }
